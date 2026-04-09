648808ef9e856f0aab653da95822df8c19343d0e
colinperry07 <colin.w.perry@proton.me>
2026-04-09 01:07:44 -0400

    feat: formatted builder.go

--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -5,20 +5,19 @@ import (
 	"strings"
 )
 
-type Option func (*Style)
+type Option func(*Style)
 
 type Style struct {
 	foreground string
 }
 
-func WithForeground (color int) Option {
-	return func (s *Style) {
+func WithForeground(color int) Option {
+	return func(s *Style) {
 		s.foreground = fmt.Sprintf("%d", color)
 	}
 }
 
-
-func Build (textToModify string, opts ...Option) string {
+func Build(textToModify string, opts ...Option) string {
 	s := &Style{}
 	for _, opt := range opts {
 		opt(s)
@@ -29,12 +28,11 @@ func Build (textToModify string, opts ...Option) string {
 	if s.foreground != "" {
 		codes = append(codes, s.foreground)
 	}
-	
+
 	if len(codes) == 0 {
 		return textToModify
 	}
 
 	return fmt.Sprintf("\x1b[%sm%s\x1b[0m", strings.Join(codes, ";"), textToModify)
-	
-}
 
+}
